ff1533c4fc54fda32be5cd4842d5afdfaf4536b1
Torvald Bringsvor <bringsvor@bringsvor.com>
2015-12-09 09:39:48 +0000

    Moved some fields to elmatica_utils to avoid circular dependency.

--- models/order.py
+++ models/order.py
@@ -8,12 +8,13 @@ from datetime import date
 
 class purchase_order_line(models.Model):
     _inherit = "purchase.order.line"
-
+    """
     @api.one
     def _compute_default_lot_year(self):
 	year = date.today().year
 	return year
-
+    """
+    """
     @api.one
     def _compute_assemble_date_code(self):
 	year = str(self.lot_year or 1)
@@ -26,19 +27,20 @@ class purchase_order_line(models.Model):
 	else:
 		batch_number = 'N/A'
 	self.assemble_date_code = str(self.lot_week or 1).zfill(2) + year + batch_number
+    """ # utils
 
-    leadtime = fields.Integer('Leadtime', default=10, help='Requested leadtime in days.')
-    units_shipped = fields.Integer('Units Shipped', help='Units Shipped',default=0)
-    weight = fields.Float('Weight', help='Weight',default=0)
-    collies = fields.Integer('# Collies', help='# Collies',default=0)
-    units_in_stock = fields.Integer('Units in Stock', help='Units in Stock',default=0)
-    batch_number = fields.Char('Lot Number', help='Batch Number',size=24,default='N/A')
-    tracking_number = fields.Char('Tracking Number', help='Batch Number',size=12,default='N/A')
-    date_code = fields.Char('Shipping Date', help='Date Code',default='1980/01/01')
-    expiry_date = fields.Char('Expiry Date', help='Expiry Date',default='1980/01/01')
-    lot_week = fields.Integer('Lot Week', help='Lot Week',default=1)
-    lot_year = fields.Integer('Lot Year', help='Lot Year',default=_compute_default_lot_year)
-    assemble_date_code = fields.Char('Assemble Date Code', help='Assemble Date Code',readonly=True,compute=_compute_assemble_date_code)
+    #leadtime = fields.Integer('Leadtime', default=10, help='Requested leadtime in days.')
+    # UTILS units_shipped = fields.Integer('Units Shipped', help='Units Shipped',default=0)
+    # UTILS :weight = fields.Float('Weight', help='Weight',default=0)
+    #collies = fields.Integer('# Collies', help='# Collies',default=0)
+    #units_in_stock = fields.Integer('Units in Stock', help='Units in Stock',default=0)
+    # UTILS batch_number = fields.Char('Lot Number', help='Batch Number',size=24,default='N/A')
+    #tracking_number = fields.Char('Tracking Number', help='Batch Number',size=12,default='N/A')
+    #date_code = fields.Char('Shipping Date', help='Date Code',default='1980/01/01')
+    #expiry_date = fields.Char('Expiry Date', help='Expiry Date',default='1980/01/01')
+    #UTILS lot_week = fields.Integer('Lot Week', help='Lot Week',default=1)
+    #lot_year = fields.Integer('Lot Year', help='Lot Year',default=_compute_default_lot_year)
+    # assemble_date_code = fields.Char('Assemble Date Code', help='Assemble Date Code',readonly=True,compute=_compute_assemble_date_code)
 
 
 class purchase_requisition(models.Model):
