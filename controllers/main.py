0ae1737b613c1dea8c715cf3bd585e2b29e3ac15
morrillo <gustavo.orrillo@moldeo.coop>
2015-12-09 14:16:55 -0300

    Added leadtime to saved columns

--- controllers/main.py
+++ controllers/main.py
@@ -209,7 +209,7 @@ class purchase_quote(http.Controller):
 		price_unit = post['price_unit'][i]
 		vals = {
 			'price_unit': price_unit,
-			#'leadtime': leadtime,
+			'leadtime': leadtime,
 			}
 	        line_id=int(line_id)
 
